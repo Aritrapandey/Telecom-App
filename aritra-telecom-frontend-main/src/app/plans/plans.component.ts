b998fe19a5875592945e0ef9840739220826dc6f
AritraPandey-Java FullStack <70154216+Aritrapandey@users.noreply.github.com>
2023-03-28 12:39:40 +0530

    Add files via upload

---
+++ aritra-telecom-frontend-main/src/app/plans/plans.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import {  OnInit  } from '@angular/core';
+
+
+@Component({
+  selector: 'app-plans',
+  templateUrl: './plans.component.html',
+  styleUrls: ['./plans.component.css']
+})
+export class PlansComponent implements OnInit {
+
+constructor(private http:HttpClient){ 
+  this.http.get("")
+}
+
+
+  ngOnInit(): void{
+
+  }
+
+}
